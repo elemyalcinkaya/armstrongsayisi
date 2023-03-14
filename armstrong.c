49694b1ed32cb52c2cc3a1f8b68528e67e11d984
elemyalcinkaya <116906661+elemyalcinkaya@users.noreply.github.com>
2023-03-14 21:46:48 +0300

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,25 @@
+# include<stdio.h>
+#include <math.h>
+int main(){
+	
+	int sayi,sum,a,islenenSayi;
+	sum=0;
+	scanf("%d",&sayi);
+	islenenSayi=sayi;
+	while(islenenSayi>0){
+		a=islenenSayi%10;
+		sum=sum+pow(a,3);
+		islenenSayi=(islenenSayi-a)/10;
+	}
+	if(sum==sayi){
+		printf("%d armstrong sayisi",sayi);
+	}
+	else{
+		printf("%d armstrong sayisi degil",sayi);
+	}
+	
+	
+	
+	return 0;
+	
+}
